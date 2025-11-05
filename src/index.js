ded6dd33f115a27f002e29d711492011c4bbdeaa
Reece <supersimon901@gmail.com>
2025-11-04 19:15:32 -0600

    adding netlify link and env variable

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your React app's URL
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
